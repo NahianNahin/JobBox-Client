84af9063f66881502b889dddac483b7654b28bf9
NahianNahin <nahian1927040@stud.kuet.ac.bd>
2023-02-16 22:19:44 +0600

    BaseUrl added

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
     reducerPath : "api",
     baseQuery : fetchBaseQuery({
-        baseUrl : "http://localhost:5000"
+        baseUrl : "https://jobbox-server-nahiannahin.vercel.app"
     }),
     tagTypes : ["jobs","apply","job"],
     endpoints : (builder) => ({}),
